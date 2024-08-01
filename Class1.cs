34105660a41312e1e9f79461daf3daffdb254544
Moumin Ahmad <116239910+Moumin-pk@users.noreply.github.com>
2024-08-01 23:26:40 +0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Calculater_in_C_Sharp
+{
+    internal class Calculater
+    {
+
+        public int sum(int a, int b)
+        {
+
+            return a + b;
+        }
+
+        public int min(int a, int b)
+        {
+
+            return a - b;
+        }
+
+        public int Div(int a, int b)
+        {
+
+            return a / b;
+        }
+
+        public int Mult(int a, int b)
+        {
+
+            return a * b;
+        }
+
+        public int Moduls(int a, int b)
+        {
+
+            return a % b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Calculater_in_C_Sharp;
+
+Calculater CalculaterObj = new Calculater();
+
+Console.WriteLine("Enter your First Number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your Second Number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your Operater( +, - , / , *)");
+string? op = Console.ReadLine();
+
+
+
+
+if(op == "+")
+{
+    Console.WriteLine($"Sum of {num1} or {num2} is = {CalculaterObj.sum(num1,num2)}");
+}
+else if (op == "-")
+{
+    Console.WriteLine($"Minis of {num1} or {num2} is = {CalculaterObj.min(num1,num2)}");
+}
+else if (op == "/")
+{
+    Console.WriteLine($"Division of {num1} or {num2} is = {CalculaterObj.Div(num1,num2)}");
+}
+else if (op == "*")
+{
+    Console.WriteLine($"Multiplay of {num1} or {num2} is = {CalculaterObj.Mult(num1,num2)}");
+}
+else if (op == "%")
+{
+    Console.WriteLine($"Moduls of {num1} or {num2} is = {CalculaterObj.Moduls(num1,num2)}");
+}
+else
+{
+    Console.WriteLine("Please Enter Valid Operater");
+}
+
+
+
+Console.WriteLine(34.40M);
